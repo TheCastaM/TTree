fe97342faf716924ab0ed6d42dbe34662a6079c4
Casta <tomasoctaviocastagnino@gmail.com>
2024-04-28 16:16:16 -0300

    Final tuches to the delete function

--- trabajo.hs
+++ trabajo.hs
@@ -14,42 +14,42 @@ data TTree k v = Node k (Maybe v) (TTree k v) (TTree k v) (TTree k v)
 -}
 
 {-
-    search devuelve el valor asociado a una clave.
+    a) search devuelve el valor asociado a una clave.
 -}
 search :: Ord k => [k] -> TTree k v -> Maybe v
 search _ E = Nothing
-search (c:cs) (Leaf k val) = if ((c == k) && (cs == [])) then Just val else Nothing
-search (c:cs) (Node k val l m r) | (c == k) = if (cs == []) then val else search cs m
-                                | (c < k) = search (c:cs) l
-                                | (c > k) = search (c:cs) r
-                                | otherwise = Nothing
+search (c:cs) (Leaf k val) = if ((c == k) && (cs == [])) 
+                                then Just val 
+                                else Nothing
+search (c:cs) (Node k val l m r)    | (c == k) = if (cs == []) then val else search cs m
+                                    | (c < k) = search (c:cs) l
+                                    | (c > k) = search (c:cs) r
+                                    | otherwise = Nothing
 
-isEmpty :: Ord k => TTree k v -> Bool
-isEmpty E = True
-isEmpty _ = False
+
+
+{-
+    crea_node crea los nodes de una clave.
+-}
+crea_node :: Ord k => [k] -> v -> TTree k v
+crea_node (c:cs) v   | cs == [] = (Leaf c v)
+                    | otherwise = (Node c Nothing E (crea_node cs v) E)
 
 {-
     insert agrega un par (clave, valor) a un arbol.
 -}
-
-{-
-    creaNode crea los nodes de una clave.
--}
-creaNode :: Ord k => [k] -> v -> TTree k v
-creaNode (c:cs) v   | cs == [] = (Leaf c v)
-                    | otherwise = (Node c Nothing E (creaNode cs v) E)
-
-
 insert :: Ord k => [k] -> v -> TTree k v -> TTree k v
-insert (c:cs) valN E = creaNode (c:cs) valN
+insert (c:cs) valN E = crea_node (c:cs) valN
 insert (c:cs) valN (Leaf k valA)    | (k == c) && (cs == []) = (Leaf k valN)
-                                    | (k == c)  = (Node k (Just valA) E (creaNode cs valN) E)
-                                    | (c < k)   = (Node k (Just valA) (creaNode (c:cs) valN) E E)
-                                    | otherwise = (Node k (Just valA) E E (creaNode (c:cs) valN))
+                                    | (k == c)  = (Node k (Just valA) E (crea_node cs valN) E)
+                                    | (c < k)   = (Node k (Just valA) (crea_node (c:cs) valN) E E)
+                                    | otherwise = (Node k (Just valA) E E (crea_node (c:cs) valN))
 insert (c:cs) valN (Node k valA l m r)  | (k == c) && (cs == []) = (Node k (Just valN) l m r)
                                         | (k == c)  = (Node k valA l (insert cs valN m) r)
                                         | (c < k)   = (Node k valA (insert (c:cs) valN l) m r)
                                         | otherwise = (Node  k valA l m (insert (c:cs) valN r))
+
+
 
 {-
     key devuelve el codigo de un nodo
@@ -73,6 +73,9 @@ middle_son E = E
 middle_son (Leaf _ _) = E
 middle_son (Node _ _ _ m _) = m
 
+{-
+    check evalua que no haya una rama que no tenga valores.
+-}
 check :: Ord k => TTree k v -> TTree k v
 check E = E
 check (Node _ Nothing E E E) = E
@@ -85,23 +88,20 @@ delete :: Ord k => [k] -> TTree k v -> TTree k v
 delete _ E = E
 delete (c:cs) (Leaf k val)  | (c == k) && (cs == []) = E
                             | otherwise = Leaf k val
-delete (c:cs) (Node k val l E E)    | (c == k) && (cs == []) = l
-                                    | (c < k) = check((Node k val (delete (c:cs) l) E E))
-                                    | otherwise = (Node k val l E E)
-delete (c:cs) (Node k val E E r)    | (c == k) && (cs == []) = r
-                                    | (c > k) = check((Node k val E E (delete (c:cs) r)))
-                                    | otherwise = (Node k val E E r)
 delete (c:cs) (Node k val l E r)    | (c == k) && (cs == []) = check((aux l r))
                                     | (c < k) = check((Node k val (delete (c:cs) l) E r))                                    
                                     | (c > k) = check((Node k val l E (delete (c:cs) r)))
                                     | otherwise = (Node k val l E r)
     where
         aux :: Ord k => TTree k v -> TTree k v -> TTree k v
+        aux l E = l
+        aux E r = r
         aux (Leaf k v) r = Node k (Just v) E E r
         aux l (Leaf k v) = Node k (Just v) l E E
-        aux (Node k lv ll lm lr) r = (Node (key(right_most lr)) 
+        aux (Node k v l m E) r = (Node k v l m r)
+        aux (Node k v l m lr) r = (Node (key(right_most lr)) 
                                             (value(right_most lr)) 
-                                            (Node k lv ll lm (righted lr))
+                                            (Node k v l m (righted lr))
                                             (middle_son(right_most lr)) 
                                             r)
         right_most :: Ord k => TTree k v -> TTree k v
@@ -110,8 +110,7 @@ delete (c:cs) (Node k val l E r)    | (c == k) && (cs == []) = check((aux l r))
         right_most (Node k v l m r) = (right_most r)
         righted :: Ord k => TTree k v -> TTree k v
         righted (Leaf _ _) = E
-        righted (Node k v l m (Leaf _ _)) = (Node k v l m E)
-        righted (Node k v l m (Node _ _ l1 _ E)) = (Node k v l m l1)
+        righted (Node k v l m E) = l
         righted (Node k v l m r) = (Node k v l m (righted r))
 delete (c:cs) (Node k val l m r)    | (c == k) && (cs == []) = check((Node k Nothing l m r))
                                     | (c == k) = check((Node k val l (delete cs m) r)) 
@@ -119,6 +118,11 @@ delete (c:cs) (Node k val l m r)    | (c == k) && (cs == []) = check((Node k Not
                                     | (c > k) = check((Node k val l m (delete (c:cs) r)))
                                     | otherwise = (Node k val l m E) 
 
+
+
+{-
+    keys devuelve un arreglo ordenado de las claves en arbol.
+-}
 
 keys :: Ord k => TTree k v -> [[k]]
 keys E = [[]]
@@ -150,13 +154,6 @@ keys (Node k Nothing l m r)  = keys(l) ++ (map (\x -> k : x) (keys m)) ++ keys r
 
 keys (Node k v l m r) = keys(l) ++  [[k]] ++ (map (\x -> k : x) (keys m)) ++ keys r
 
-t = Node 'r' Nothing E (Node 'e' (Just 16) (Node 'a' Nothing E (Leaf 's' 1) E)(Node 'o' (Just 2) (Leaf 'd' 9)E(Leaf 's' 4))E)(Node 's' Nothing E (Node 'i' (Just 4) (Leaf 'e' 8)(Leaf 'n' 7)E)E)
-
-
-
-
-
-
 
 class Dic k v d | d -> k v where
     vacio :: d
@@ -166,17 +163,10 @@ class Dic k v d | d -> k v where
     claves :: Ord k => d -> [k]
 
 instance Ord k => Dic [k] v (TTree k v) where
-  -- Diccionario vacío
   vacio = E
+  insertar = insert
+  buscar = search  
+  eliminar = delete
+  claves = keys
 
-  -- Insertar un par clave-valor
-  insertar = insert  -- Use key directly (no list)
-
-  -- Buscar el valor asociado a una clave
-  buscar = search  -- Use key directly (no list), fixed syntax error
-
-  -- Eliminar una clave y su valor asociado
-  eliminar = delete   -- Use key directly (no list)
-
-  -- Obtener todas las claves del árbol
-  claves = keys  -- Return all keys from TTree's keys function
+t = Node 'r' Nothing E (Node 'e' (Just 16) (Node 'a' Nothing E (Leaf 's' 1) E)(Node 'o' (Just 2) (Leaf 'd' 9)E(Leaf 's' 4))E)(Node 's' Nothing E (Node 'i' (Just 4) (Leaf 'e' 8)(Leaf 'n' 7)E)E)
